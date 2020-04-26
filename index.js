20435c4830f456c694e4ceaeb4cd9578878ed580
Christian Steimel <me@C14L.com>
2020-04-26 14:30:08 +0200

    Add lots of sync

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const elems = {
     table: document.querySelector("#table"),
     ball: document.querySelector("#table > .ball"),
     numbers: {
+        score: {
+            left: document.querySelector("#numbers > .score > .left"),
+            right: document.querySelector("#numbers > .score > .right"),
+        },
         websocket: document.querySelector("#numbers > .websocket"),
         players: document.querySelector("#numbers > .players"),
         velocity: {
@@ -24,12 +28,23 @@ const settings = {
     drag: 0.8,
     mapBarPlayer: [[0], [1, 2], [3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18], [19, 20], [21]], // mapping of "player" objects onto "bar" objects
     kickSpeed: [10, 10], // default "kick" acceleration
+    debugShowNumbers: true,
 };
 
 const items = {
-    ball: { radius: elems.ball.offsetWidth / 2, vx: 0, vy: 0, ax: 0, ay: 0, vvec: 0, avec: 0, },
-    table: resizeTableAndGetBoundingClientRect(),
-    goals: resizeGoalsAndGetBoundingClientRect(),
+    table: resizeTableAndGetDOMRect(),
+    goals: resizeGoalsAndGetDOMRect(),
+    ball: {
+        radius: elems.ball.offsetWidth / 2,
+        x: 0,
+        y: 0,
+        vx: 0,
+        vy: 0,
+        ax: 0,
+        ay: 0,
+        vvec: 0,
+        avec: 0,
+    },
     bars: {
         players: [],
         limits: [],
@@ -52,11 +67,13 @@ const status = {
     kickBars: [0, 0, 0, 0, 0, 0, 0, 0], // activate a bar to kick against a ball
     playerMovement: { "up": -2, "down": 2 },
     playerBars: { "left": 0, "right": 1 }, // this player's own bars controlled with "left" and "right" hand
-    goalCount: { "left": 0, "right": 0 },
+    score: { "left": 0, "right": 0 },
     leftMoveUpInterval: false,
     leftMoveDownInterval: false,
     rightMoveUpInterval: false,
     rightMoveDownInterval: false,
+    animationInterval: null,
+    ballSyncInterval: null,
 };
 
 ws = new WebSocket("ws://localhost:8000/");
@@ -67,64 +84,48 @@ window.addEventListener("keyup", handleKeyUp);
 window.addEventListener("keydown", handleKeyDown);
 window.addEventListener("keypress", handleKeyPress);
 
+function wsSend(action, data) {
+    data = data || {};
+    data.action = action;
+    ws.send(JSON.stringify(data));
+}
+
 function handleWebsocketOpen(event) {
     console.log("handleWebsocketOpen", event.data);
     elems.numbers.websocket.style.backgroundColor = 'yellow';
-    ws.send(JSON.stringify({
-        "action": "register",
-        "game": settings.gameId,
-        "user": settings.userId,
-    }));
+    wsSend("register", { "game": settings.gameId, "user": settings.userId });
 }
 
 function handleWebsocketClose(event) {
-    elems.numbers.websocket.style.backgroundColor = 'black';
     console.log("handleWebsocketClose", event.data);
-    ws.send(JSON.stringify({
-        "action": "register",
-        "game": settings.gameId,
-        "user": settings.userId,
-    }));
+    writeWsStatus('black');
+    // TODO: is this needed?? wsSend("register", { "game": settings.gameId, "user": settings.userId });
 }
 
 function handleWebsocketMessage(event) {
-    elems.numbers.websocket.style.backgroundColor = 'blue';
     console.log("handleWebsocketMessage", event.data);
+    writeWsStatus('blue');
     let response = JSON.parse(event.data);
 
     if (response.action == "playerlist") {
-        elems.numbers.players.innerHTML = response.playerlist.join(", ");
+        writePlayers(response.playerlist);
+
         if (response.playerlist.length == 1) {
-            // This player was the first to join, so this is the master player
             settings.isServer = true;
         }
-        if (response.playerlist.length == settings.playerLimit) {
-            // All four players have joined the game
-            if (settings.isServer) {
-                // If this is the master player, tell others to sync items
-                resetGame();
-                ws.send(JSON.stringify({
-                    "action": "sync",
-                    "hold": status.hold,
-                    "table": items.table,
-                    "goals": items.goals,
-                    "ball": items.ball,
-                    "player": items.player,
-                    "bars": items.bars,
-                    "players": {},  // TODO
-                }));
-            }
-        }
         if (response.playerlist.length < settings.playerLimit) {
-            console.log("Waiting for more players:", response.playerlist);
+            // console.log("Waiting for more players:", response.playerlist);
+        }
+        if (response.playerlist.length == settings.playerLimit && settings.isServer) {
+            resetGame();
         }
         if (response.playerlist.length > settings.playerLimit) {
             alert("Error, too many players. How did that happen?! o.O");
         }
     }
 
-    if (response.action == "sync") {
-        console.log("Received 'sync' action with 'items' being: ", items);
+    if (response.action == "gamesync") {
+        // console.log("Received 'gamesync' action with 'items' being: ", items);
         // Request to sync with master
         items.table = response.table;
         items.goals = response.goals;
@@ -132,77 +133,75 @@ function handleWebsocketMessage(event) {
         items.player = response.player;
         items.bars = response.bars;
         //items.players = response.players;
-        console.log("Updated items due to 'sync' action to: ", items);
-
-        elems.table.style.right = "auto";
-        elems.table.style.bottom = "auto";
-        elems.table.style.top = items.table.top + "px";
-        elems.table.style.left = items.table.left + "px";
-        elems.table.style.width = items.table.width + "px";
-        elems.table.style.height = items.table.height + "px";
-
-        elems.goals.forEach((elG, i) => {
-            elems.goals[i].style.right = "auto";
-            elems.goals[i].style.bottom = "auto";
-            elems.goals[i].style.top = items.goals[i].top + "px";
-            elems.goals[i].style.left = items.goals[i].left + "px";
-            elems.goals[i].style.width = items.goals[i].width + "px";
-            elems.goals[i].style.height = items.goals[i].height + "px";
-        });
-
-        // Sync finished!
-        ws.send(JSON.stringify({ "action": "syncok" }));
+        // console.log("Updated items due to 'gamesync' action to: ", items);
+        resizeTableAndGetDOMRect(items.table);
+        resizeGoalsAndGetDOMRect(items.goals);
+        wsSend("syncok");
     }
 
     if (response.action == "syncok") {
         // A sync was finished, count players ready
         status.syncOkCount++;
-        elems.numbers.websocket.style.innerHTML = status.syncOkCount;
+        writeWsConnectCount();
         if (settings.isServer && status.syncOkCount == settings.playerLimit) {
             // If master and all 4 players are ready, start the game
-            ws.send(JSON.stringify({ "action": "play" }));
+            wsSend("playprepare");
         }
     }
 
-    if (response.action == "play") {
-        elems.numbers.websocket.style.backgroundColor = 'green';
-        gamePlay().then(result => {
-            if (settings.isServer) {
-                ws.send(JSON.stringify({ "action": "newgoal", "goalfor": result }));
-            }
-        });
+    if (response.action == "playprepare") {
+        writeWsStatus('green');
+        writeScore();
+        if (settings.isServer) {
+            randomizeBall();
+            wsSend("playstart", { "ball": items.ball });
+        }
+    }
+
+    if (response.action == "playstart") {
+        items.ball = response.ball;
+        gamePlay();
+    }
+
+    if (response.action == "ballsync") {
+        items.ball = response.ball;
     }
 
     if (response.action == "newgoal") {
-        status.goalCount[response.goalfor]++;
+        status.goalHit = true;
+        clearInterval(status.animationInterval);
+        clearInterval(status.ballSyncInterval);
+        status.animationInterval = null;
+        status.ballSyncInterval = null;
+        // console.log("Goal hit, interval canceled.");
 
         if (settings.isServer) {
-            if (status.goalCount.left >= 8 || status.goalCount.rigth >= 8) {
-                ws.send(JSON.stringify({ "action": "finish", "winner": response.goalfor }));
+            status.score[response.goalfor]++;
+            wsSend("statusupdate", { "key": "score", "val": status.score });
+            if (status.score.left >= 8 || status.score.rigth >= 8) {
+                wsSend("finish", { "winner": response.goalfor });
             } else {
                 resetGame();
-                ws.send(JSON.stringify({
-                    "action": "sync",
-                    "table": items.table,
-                    "goals": items.goals,
-                    "ball": items.ball,
-                    "player": items.player,
-                    "bars": items.bars,
-                    "players": {},  // TODO
-                }));
             }
         }
     }
 
     if (response.action == "statusupdate") {
         status[response.key] = response.val;
-        console.log("New status set key, value:", response.key, response.val);
+        // console.log("New status set key, value:", response.key, response.val);
+        writeNumbers();
     }
 
     if (response.action == "finish") {
         // End of game by goal count
         alert("Winner is: ", response.winner);
         status.gameOver = true;
+    }
+}
+
+function wsSyncBall() {
+    if (settings.isServer) {
+        wsSend("ballsync", { "ball": items.ball });
     }
 }
 
@@ -240,9 +239,13 @@ function handleKeyDown(event) {
 }
 
 function handleKeyPress(event) {
-    if (event.key == "p") {
+    if (event.key == "p") { // Pause game
         status.hold = !status.hold;
-        ws.send(JSON.stringify({ "action": "statusupdate", "key": "hold", "val": status.hold }));
+        wsSend("statusupdate", { "key": "hold", "val": status.hold });
+    }
+    if (event.key == "b") { // Add random velocity to ball
+        randomizeBall();
+        wsSend("playstart", { "ball": items.ball });
     }
 }
 
@@ -253,72 +256,49 @@ function moveBar(barIndex, dir) {
     window.requestAnimationFrame(() => elems.bars[barIndex].style.top = newTop + "px");
 }
 
-function getCenterXY(elem) {
-    const b = saneDOMRect(elem);
-    return [b.x, b.y];
-}
-
-function writeNumbers() {
-    elems.numbers.velocity.t.innerHTML = Math.round(ballVVec() * 1000) / 1000;
-    elems.numbers.velocity.x.innerHTML = Math.round(items.ball.x * 1000) / 1000;
-    elems.numbers.velocity.y.innerHTML = Math.round(items.ball.y * 1000) / 1000;
-}
-
 function gamePlay() {
-    return new Promise((resolve, reject) => {
-        let animationInterval = null;
-        let goalCollission = null;
+    let goalCollission = null;
 
-        gameInit();
+    status.ballSyncInterval = setInterval(() => wsSyncBall(), 1000);
 
-        animationInterval = setInterval(() => {
-            if ( ! status.hold ) {
-                moveBall();
-                handlePlayerCollission();
-                handleWallCollission();
+    status.animationInterval = setInterval(() => {
+        if (!status.hold) {
+            moveBall();
+            handlePlayerCollission();
+            handleWallCollission();
+
+            if (settings.isServer) {
+                goalCollission = getGoalCollission();
+                if (goalCollission) {
+                    wsSend("newgoal", { "goalfor": goalCollission });
+                }
             }
+        }
 
-            window.requestAnimationFrame(() => {
-                writeNumbers();
-
-                const pos = getCenterXY(elems.ball);
-                elems.ball.innerHTML = Math.round(pos[0]) + " " + Math.round(pos[1]);
-
-                elems.players.forEach(p => {
-                    const pos = getCenterXY(p);
-                    p.innerHTML = Math.round(pos[0]) + " " + Math.round(pos[1]);
-                });
-
-                drawBall();
-            });
-
-            goalCollission = getGoalCollission();
-            if (goalCollission) {
-                status.goalHit = true;
-                clearInterval(animationInterval);
-                animationInterval = null;
-                console.log("Goal hit, interval canceled.");
-                return goalCollission;
-            }
-        }, 10);
-    });
+        window.requestAnimationFrame(() => {
+            drawBall();
+            writeNumbers();
+            writeNumbersInBall();
+            writeNumbersInPlayers();
+        });
+    }, 20);
 }
 
 function getGoalCollission(ball) {
     if (items.ball.y > items.goals[0].top && items.ball.y < items.goals[0].bottom) {
-        if (items.ball.x >= items.ball.xMax) return "right";
-        if (items.ball.x <= items.ball.xMin) return "left";
+        if (items.ball.x >= (items.table.right - items.ball.radius)) return "right";
+        if (items.ball.x <= (items.table.left + items.ball.radius)) return "left";
     }
     return null;
 }
 
 function handlePlayerCollission() {
-    console.log("handlePlayerCollission() called.");
+    // console.log("handlePlayerCollission() called.");
     const ft = (items.ball.x <= items.table.x) ? [0, 3] : [4, 7];
     const c = Math.pow(items.ball.radius + items.player.radius, 2);
 
     for (let i = ft[0]; i <= ft[1]; i++) {
-        const barx = getCenterXY(elems.bars[i])[0];
+        const barx = saneDOMRect(elems.bars[i]).x;
 
         if (items.ball.x + items.ball.radius > barx - items.player.radius && items.ball.x - items.ball.radius < barx + items.player.radius) {
             elems.bars[i].classList.add("impact");
@@ -329,12 +309,12 @@ function handlePlayerCollission() {
                 /* jshint +W083 */
                 const player = saneDOMRect(elems.players[pi]);
                 const ab = Math.pow(items.ball.x - player.x, 2) + Math.pow(items.ball.y - player.y, 2);
-                console.log("ab", ab);
+                // console.log("ab", ab);
 
                 if (ab <= c) {
                     elems.players[pi].classList.add("impact");
                     setTimeout(() => { elems.players[pi].classList.remove("impact"); }, 100);
-                    console.log("ball is in player range");
+                    // console.log("ball is in player range");
                     handleBallDeflection(player);
 
                     if (status.kickBars[pi]) {
@@ -365,25 +345,37 @@ function handleBallDeflection(player) {
 }
 
 function handleWallCollission() {
-    if (items.ball.x < items.ball.xMin && items.ball.vx < 0) { items.ball.vx *= -1; console.log("ball wall collission", items.ball); }
-    if (items.ball.y < items.ball.yMin && items.ball.vy < 0) { items.ball.vy *= -1; console.log("ball wall collission", items.ball); }
-    if (items.ball.x > items.ball.xMax && items.ball.vx > 0) { items.ball.vx *= -1; console.log("ball wall collission", items.ball); }
-    if (items.ball.y > items.ball.yMax && items.ball.vy > 0) { items.ball.vy *= -1; console.log("ball wall collission", items.ball); }
+    if (items.ball.vx < 0 && items.ball.x < (items.table.left + items.ball.radius)) {
+        items.ball.vx *= -1;
+    }
+    if (items.ball.vy < 0 && items.ball.y < (items.table.top + items.ball.radius)) {
+        items.ball.vy *= -1;
+    }
+    if (items.ball.vx > 0 && items.ball.x > (items.table.right - items.ball.radius)) {
+        items.ball.vx *= -1;
+    }
+    if (items.ball.vy > 0 && items.ball.y > (items.table.bottom - items.ball.radius)) {
+        items.ball.vy *= -1;
+    }
 }
 
-function resizeTableAndGetBoundingClientRect() {
-    let table = elems.table.getBoundingClientRect();
+function resizeTableAndGetDOMRect(table) {
+    table = table || elems.table.getBoundingClientRect();
     elems.table.style.top = table.top + "px";
+    elems.table.style.right = "auto";
+    elems.table.style.bottom = "auto";
     elems.table.style.left = table.left + "px";
     elems.table.style.width = table.width + "px";
     elems.table.style.height = table.height + "px";
     return saneDOMRect(elems.table);
 }
 
-function resizeGoalsAndGetBoundingClientRect() {
+function resizeGoalsAndGetDOMRect(goals) {
     elems.goals.forEach((elG, i) => {
-        let goal = elG.getBoundingClientRect();
+        let goal = goals && goals[i] || elems.goals[i].getBoundingClientRect();
         elems.goals[i].style.top = goal.top + "px";
+        elems.goals[i].style.right = "auto";
+        elems.goals[i].style.bottom = "auto";
         elems.goals[i].style.left = goal.left + "px";
         elems.goals[i].style.width = goal.width + "px";
         elems.goals[i].style.height = goal.height + "px";
@@ -402,7 +394,7 @@ function saneDOMRect(elem) {
         bottom: domRect.bottom,
         left: domRect.left,
         width: domRect.width,
-        height: domRect.heigh,
+        height: domRect.height,
         x: domRect.width / 2 + domRect.left, // Center point of the object
         y: domRect.height / 2 + domRect.top, // Center point of the object
     };
@@ -434,24 +426,67 @@ function drawBall() {
 function resetGame() {
     // After a goal, reset some status and item properties
     status.goalHit = false;
-
-    items.ball.x = items.table.left + items.ball.radius;
-    items.ball.y = items.table.top + items.ball.radius;
-
-    items.ball.xMin = items.table.left + items.ball.radius;
-    items.ball.yMin = items.table.top + items.ball.radius;
-    items.ball.xMax = items.table.right - items.ball.radius;
-    items.ball.yMax = items.table.bottom - items.ball.radius;
-
+    items.ball.x = items.table.x;
+    items.ball.y = items.table.y;
     items.ball.vx = 0;
     items.ball.vy = 0;
     items.ball.ax = 0;
     items.ball.ay = 0;
+
+    wsSend("gamesync", {
+        "hold": status.hold,
+        "table": items.table,
+        "goals": items.goals,
+        "ball": items.ball,
+        "player": items.player,
+        "bars": items.bars,
+        "players": {},  // TODO
+    });
 }
 
-function gameInit() {
+function randomizeBall() {
     items.ball.vx = Math.random() * 20;
     items.ball.vy = Math.random() * 20;
     items.ball.ax = 0.995;
     items.ball.ay = 0.995;
+}
+
+function writeScore() {
+    elems.numbers.score.left.innerHTML = status.score.left;
+    elems.numbers.score.right.innerHTML = status.score.right;
+}
+
+function writePlayers(playerlist) {
+    elems.numbers.players.innerHTML = playerlist.join(", ");
+}
+
+function writeNumbers() {
+    elems.numbers.velocity.t.innerHTML = Math.round(ballVVec() * 1000) / 1000;
+    elems.numbers.velocity.x.innerHTML = Math.round(items.ball.x * 1000) / 1000;
+    elems.numbers.velocity.y.innerHTML = Math.round(items.ball.y * 1000) / 1000;
+}
+
+function writeWsConnectCount() {
+    elems.numbers.websocket.innerHTML = status.syncOkCount;
+}
+
+function writeWsStatus(s) {
+    elems.numbers.websocket.style.backgroundColor = s;
+    if (settings.isServer) {
+        elems.numbers.websocket.style.border = "2px solid white";
+    }
+}
+
+function writeNumbersInPlayers() {
+    elems.players.forEach(p => {
+        const pos = saneDOMRect(p);
+        p.innerHTML = Math.round(pos.x) + " " + Math.round(pos.y);
+    });
+}
+
+function writeNumbersInBall() {
+    if (settings.debugShowNumbers) {
+        const pos = saneDOMRect(elems.ball);
+        elems.ball.innerHTML = Math.round(pos.x) + " " + Math.round(pos.y);
+    }
 }
